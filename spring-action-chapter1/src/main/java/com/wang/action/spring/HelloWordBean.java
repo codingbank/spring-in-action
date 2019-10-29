ccb36470b413c47867cabb03a1144baf6f169aa5
王佳辉 <wangjiahui@edianzu.com>
2019-10-29 13:20:21 +0800

    add class HelloWordBean

---
+++ spring-action-chapter1/src/main/java/com/wang/action/spring/HelloWordBean.java
@@ -0,0 +1,16 @@
+package com.wang.action.spring;
+
+/**
+ * @author [wangjiahui]
+ * @ClassName: HelloWordBean
+ * @CreateDate: [2019-10-29 13:19]
+ * @Description: [TODO]
+ * @version: [V1.0]
+ */
+public class HelloWordBean {
+
+    public String sayHello(){
+        return "Hello Word";
+    }
+
+}
